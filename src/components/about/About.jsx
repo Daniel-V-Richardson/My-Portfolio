4384bac6b64c4d3fd57043e85d25c4701fb04fa0
Daniel-V-Richardson <richardson20june@gmail.com>
2022-08-28 13:59:49 +0530

    28/8/22

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -35,7 +35,7 @@ const About = () => {
             <article className="about__card">
               <VscFolderLibrary className="about__icon" />
               <h5>Projects</h5>
-              <small>12+ Completed</small>
+              <small>6+ Completed</small>
             </article>
           </div>
           <p>
